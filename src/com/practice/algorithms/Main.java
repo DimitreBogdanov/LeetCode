0bd8ea18d70e1ba798e5a1ff616f36637f16b6c7
dimitrebogdanov <dimitre_7@hotmail.com>
2017-10-24 14:02:43 -0400

    Gitignore update and Two Sums problem with solution

--- src/com/practice/algorithms/Main.java
+++ src/com/practice/algorithms/Main.java
@@ -1,8 +1,11 @@
 package com.practice.algorithms;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        for(int i: Problems.twoSum(new int[]{2, 7, 11, 15}, 9)) {
+            System.out.println(i);
+        }
     }
 }
