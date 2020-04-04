e871670b4446a1ad575345de7032b08fff31ed3a
dimitrebogdanov <dimitre_7@hotmail.com>
2020-04-04 15:07:08 -0400

    Update progress

--- src/main/java/com/practice/algorithms/Problems/Hard.java
+++ src/main/java/com/practice/algorithms/Problems/Hard.java
@@ -1,9 +1,47 @@
 package com.practice.algorithms.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hard {
 
 
     /**
      * Hard section
      */
+
+    //https://leetcode.com/problems/text-justification/
+    //Assumptions:
+    // 0 < word.length <= maxWidth
+    // words.length > 0
+    // !word.contain(\s)
+    //
+    public static List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+
+        List<String> temp = new ArrayList<>(words.length);
+        int currentLength = 0;
+        for (String word : words) {
+            //Need to take into consideration adding at least 1 space in between, todo
+            if (currentLength + word.length() > maxWidth){
+                result.add(determineLinePack(temp, maxWidth));
+            }else {
+                //here we could reduce the length by keeping track of how many words we have removed
+                temp = new ArrayList<>(word.length());
+                temp.add(word);
+            }
+        }
+        //At the end need to calculate
+
+        return result;
+    }
+
+    char SPACE = ' ';
+    private static String determineLinePack(List<String> words, int maxWidth) {
+        StringBuilder sb = new StringBuilder();
+
+
+
+        return sb.toString();
+    }
 }
--- test/com/practice/algorithms/HardTest.java
+++ test/com/practice/algorithms/HardTest.java
@@ -1,4 +1,47 @@
 package com.practice.algorithms;
 
+import static org.junit.Assert.*;
+
+import com.practice.algorithms.Problems.Hard;
+import org.junit.Test;
+
 public class HardTest {
+
+    @Test
+    public void textJustificationTest() {
+        assertArrayEquals(
+                new String[]{
+                        "This    is    an",
+                        "example  of text",
+                        "justification.  "
+                },
+                Hard.fullJustify(
+                    new String[]{"This", "is", "an", "example", "of", "text", "justification."},16
+                ).toArray()
+        );
+        assertArrayEquals(
+                new String[]{
+                        "What   must   be",
+                        "acknowledgment  ",
+                        "shall be        "
+                },
+                Hard.fullJustify(
+                    new String[]{"What","must","be","acknowledgment","shall","be"},16
+                ).toArray()
+        );
+        assertArrayEquals(
+                new String[]{
+                        "Science  is  what we",
+                        "understand      well",
+                        "enough to explain to",
+                        "a  computer.  Art is",
+                        "everything  else  we",
+                        "do                  "
+                },
+                Hard.fullJustify(
+                    new String[]{"Science","is","what","we","understand","well","enough","to","explain","to","a",
+                        "computer.","Art","is","everything","else","we","do"},16
+                ).toArray()
+        );
+    }
 }
